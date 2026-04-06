b0cee03d0e3ce4a93cbf21ec6ed0ce621fba6ad7
zaimrahman652-tech <zaimrahman652@gmail.com>
2026-04-06 10:46:12 +0700

    15

--- arrayS.cpp
+++ arrayS.cpp
@@ -29,6 +29,9 @@ int main()
     cout << "data pertama: " << datamu[0] << endl;
     cout << "data kedua: " << datamu[1] << endl;
   z  cout << "data ketiga: " << datamu[2] << endl;
-    
+    cout << "data keempat: " << datamu[3] << endl;
+    cout << "data kelima: " << datamu[4] << endl;
 
+    return 0;
+}
     
