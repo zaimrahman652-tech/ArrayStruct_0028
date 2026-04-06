9e93d9bd920ed7159418e52e8fd9baf59584ce82
zaimrahman652-tech <zaimrahman652@gmail.com>
2026-04-06 10:55:33 +0700

    17

--- struct.cpp
+++ struct.cpp
@@ -3,3 +3,12 @@
 
 int main() 
 {
+    // deklarasi variabel struct
+    mahasiswa mhs;
+    // mrngisi data
+    cout << "isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "isikan data alamat : ";
+    cin >> mhs.alamat;
+    cout << "isikan data usia : ";
+    cin >> mhs.umur;
